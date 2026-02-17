8f2a28b2d9585c0f7fb959ba8f28c77b909c8b0b
AdityaVsVik <typius.gamer@gmail.com>
2026-02-16 18:54:36 -0600

    Fixed up 12 ball close

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Pedro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Pedro.java
@@ -6,21 +6,27 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.TelemetryManager;
 import com.bylazar.telemetry.PanelsTelemetry;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Poses;
+import org.firstinspires.ftc.teamcode.Subsystems.Robot;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.paths.PathChain;
 import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name = "Pedro Pathing Autonomous", group = "Autonomous")
 @Configurable // Panels
 public class Pedro extends OpMode {
     private TelemetryManager panelsTelemetry; // Panels Telemetry instance
-    public Follower follower; // Pedro Pathing follower instance
+    private Follower follower; // Pedro Pathing follower instance
     private int pathState; // Current autonomous path state (state machine)
     private Paths paths; // Paths defined in the Paths class
+    private ElapsedTime pathTimer, actionTimer, opmodeTimer;
 
+    Robot robot;
     @Override
     public void init() {
         panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
@@ -32,25 +38,55 @@ public class Pedro extends OpMode {
 
         panelsTelemetry.debug("Status", "Initialized");
         panelsTelemetry.update(telemetry);
+
+        pathTimer = new ElapsedTime();
+        opmodeTimer = new ElapsedTime();
+        opmodeTimer.reset();
+        follower = Constants.createFollower(hardwareMap);
+        robot =new Robot(hardwareMap,new Pose(72, 8, Math.toRadians(90)), Poses.redGoal);
+    }
+    @Override
+    public void init_loop() {}
+
+    @Override
+    public void start() {
+        opmodeTimer.reset();
+        setPathState(0);
     }
 
     @Override
     public void loop() {
-        follower.update(); // Update Pedro Pathing
-        autonomousPathUpdate(); // Update autonomous state machine
-
-        // Log values to Panels and Driver Station
-        panelsTelemetry.debug("Path State", pathState);
-        panelsTelemetry.debug("X", follower.getPose().getX());
-        panelsTelemetry.debug("Y", follower.getPose().getY());
-        panelsTelemetry.debug("Heading", follower.getPose().getHeading());
-        panelsTelemetry.update(telemetry);
+        follower.update();
+        autonomousPathUpdate();
+        if (pathState >= 0) {
+            robot.turret.setTargetAngle(
+                    robot.turret.autoAim(robot.drive.getPose(), Poses.redGoal)
+            );
+            robot.flywheels.setTargetRPM(3200);
+            robot.hood.setPosition(
+                    robot.hood.distanceToRPM(robot.drive.getPose(), Poses.redGoal)
+            );
+        } else {
+            // Final safe state
+            robot.turret.setTargetAngle(0);
+            robot.flywheels.setTargetRPM(0);
+            robot.gate.gateClosed();
+        }
+        robot.turret.update();
+        robot.flywheels.update();
+        // Feedback to Driver Hub for debugging
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
     }
 
 
 
     public static class Paths {
         public PathChain Path1;
+
         public PathChain Path2;
         public PathChain Path3;
         public PathChain Path4;
@@ -150,38 +186,62 @@ public class Pedro extends OpMode {
     public void autonomousPathUpdate() {
         switch (pathState){
             case 0:
-                follower.followPath(paths.Path1);
-                pathState = 1;
+                if (advance()){
+                follower.followPath(paths.Path1, true);
+                setPathState(1);
+                }
                 break;
             case 1:
-                follower.followPath(paths.Path2);
-                pathState = 2;
+                if (advance()) {
+                    follower.followPath(paths.Path2, true);
+                    setPathState(2);
+                }
                 break;
             case 2:
-                follower.followPath(paths.Path3);
-                pathState = 3;
+                if (advance()) {
+                    follower.followPath(paths.Path3, true);
+                    setPathState(3);
+                }
                 break;
             case 3:
-                follower.followPath(paths.Path4);
-                pathState = 4;
+                if (advance()) {
+                    follower.followPath(paths.Path4, true);
+                    setPathState(4);
+                }
                 break;
             case 4:
-                follower.followPath(paths.Path5);
-                pathState = 5;
+                if (advance()) {
+                    follower.followPath(paths.Path5, true);
+                    setPathState(5);
+                }
                 break;
             case 5:
-                follower.followPath(paths.Path6);
-                pathState = 6;
+                if (advance()) {
+                    follower.followPath(paths.Path6, true);
+                    setPathState(6);
+                }
                 break;
             case 6:
-                follower.followPath(paths.Path7);
-                pathState = 7;
+                if (advance()) {
+                    follower.followPath(paths.Path7, true);
+                    setPathState(7);
+                }
                 break;
             case 7:
-                follower.followPath(paths.Path8);
-                pathState = -1;
+                if (advance()) {
+                    follower.followPath(paths.Path8, true);
+                    setPathState(-1);
+                }
                 break;
         }
 
+    }
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.reset();
+    }
+
+    private boolean advance() {
+        return !follower.isBusy() || pathTimer.seconds() > 8;
     }
 }
